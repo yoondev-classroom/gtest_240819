d9dfcfec848e7390ce11a64f67bd70fbd9a6d90f
Chansik Yun <chansigi@gmail.com>
2024-08-19 23:12:17 +0000

    .

--- 1_시작.cpp
+++ 1_시작.cpp
@@ -7,8 +7,8 @@
 #if 0
 int main(int argc, char** argv)
 {
-	testing::InitGoogleTest(&argc, argv);
+    testing::InitGoogleTest(&argc, argv);
 
-	return RUN_ALL_TESTS();
+    return RUN_ALL_TESTS();
 }
 #endif
