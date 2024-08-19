f6d9c8fb55183610ca1e3a9c644d43454c1e941c
Chansik Yun <chansigi@gmail.com>
2024-08-19 05:42:21 +0000

    .

--- 5_전역픽스쳐.cpp
+++ 5_전역픽스쳐.cpp
@@ -26,13 +26,44 @@ public:
     }
 };
 
+class MyEnvironment2 : public testing::Environment {
+public:
+    void SetUp() override
+    {
+        std::cout << "MyEnvironment2.SetUp()" << std::endl;
+    }
+
+    void TearDown() override
+    {
+        std::cout << "MyEnvironment2.TearDown()" << std::endl;
+    }
+};
+
 // 2) 설치하는 방법 2가지
-//    - main을 직접 정의한 경우
+//     => main 함수를 통해 직접 정의하는 것이 안전합니다.
+
+//      - main을 직접 정의한 경우
+//      : 정적 라이브러리에서 main을 제거해주어야 합니다.
+//        이전 컴파일러에서는 링크 오류가 발생할 수 있습니다.
+#if 1
 int main(int argc, char** argv)
 {
     testing::InitGoogleTest(&argc, argv);
 
-    testing::AddGlobalTestEnvironment(new MyEnvironment); // !!!
+    testing::AddGlobalTestEnvironment(new MyEnvironment);
+    testing::AddGlobalTestEnvironment(new MyEnvironment2);
 
     return RUN_ALL_TESTS();
 }
+#endif
+
+//   - main을 직접 제공하지 않는 경우
+// => 전역 변수의 초기화가 main 이전에 수행되는 원리를 이용합니다.
+//  C++ 표준에서는 전역 변수의 초기화가 각 파일의 단위에서 어떤 순서로 수행될지 정의되어 있지 않습니다.
+
+#if 0
+// test1.cpp
+testing::Environment* my_env1 = testing::AddGlobalTestEnvironment(new MyEnvironment);
+// test2.cpp
+testing::Environment* my_env2 = testing::AddGlobalTestEnvironment(new MyEnvironment);
+#endif
