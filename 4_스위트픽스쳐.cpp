e2c3dd17bc1086c148de3aab06aef1acdbff2099
Chansik Yun <chansigi@gmail.com>
2024-08-19 04:52:20 +0000

    .

--- 4_스위트픽스쳐.cpp
+++ 4_스위트픽스쳐.cpp
@@ -18,6 +18,7 @@ public:
 #include <gtest/gtest.h>
 
 // 아래의 코드를 암묵적 설치/해체의 코드로 변경해보세요.
+#if 0
 TEST(TerminalTest, LogIn)
 {
     Terminal* ts = new Terminal;
@@ -27,6 +28,7 @@ TEST(TerminalTest, LogIn)
 
     ASSERT_TRUE(ts->IsLogin()) << "로그인 하였을 때";
 
+    ts->Disconnect();
     delete ts;
 }
 
@@ -40,5 +42,38 @@ TEST(TerminalTest, LogOut)
 
     ASSERT_FALSE(ts->IsLogin()) << "로그아웃 하였을 때";
 
+    ts->Disconnect();
     delete ts;
+}
+#endif
+
+class TerminalTest : public testing::Test {
+protected:
+    Terminal* ts;
+    void SetUp() override
+    {
+        Terminal* ts = new Terminal;
+        ts->Connect();
+    }
+
+    void TearDown() override
+    {
+        ts->Disconnect();
+        delete ts;
+    }
+};
+
+TEST_F(TerminalTest, LogIn)
+{
+    ts->LogIn("test_id", "test_password");
+
+    ASSERT_TRUE(ts->IsLogin()) << "로그인 하였을 때";
+}
+
+TEST_F(TerminalTest, LogOut)
+{
+    ts->LogIn("test_id", "test_password");
+    ts->LogOut();
+
+    ASSERT_FALSE(ts->IsLogin()) << "로그아웃 하였을 때";
 }
