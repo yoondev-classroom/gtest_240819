b54cbb02bc4b710cd8c278661252cab8f36eca1a
Chansik Yun <chansigi@gmail.com>
2024-08-20 07:45:55 +0000

    .

--- 17_Mock.cpp
+++ 17_Mock.cpp
@@ -49,3 +49,33 @@ public:
 //  => Mock Framework을 이용해서, 모의 객체(Mock Object)를 만들 수 있습니다.
 
 // Google Test 1.8 버전 이후로, Google Mock 프로젝트가 Google Test에 흡수되었습니다.
+
+#include <gmock/gmock.h>
+// 위의 헤더를 통해 gtest/gtest.h도 포함되기 때문에, 별도로 포함할 필요가 없습니다.
+
+// Google Mock을 이용해서 모의 객체를 만듭니다. => Mocking
+class MockDLoggerTarget : public DLoggerTarget {
+public:
+    // void Write(Level level, const std::string& message)
+    // => 행위 기반 검증을 수행하고자 하는 메소드에 대해서, MOCK_METHOD를 해야 합니다.
+
+    // MOCK_METHOD{인자개수}(메소드 이름, 메소드 타입)
+    MOCK_METHOD2(Write, void(Level level, const std::string& message));
+};
+
+TEST(DLoggerTest, Write)
+{
+    DLogger logger;
+    MockDLoggerTarget t1, t2;
+    logger.AddTarget(&t1);
+    logger.AddTarget(&t2);
+
+    logger.Write(INFO, "test_message1");
+    logger.Write(WARN, "test_message2");
+
+    // 행위 기반 검증 단언문
+    EXPECT_CALL(t1, Write(INFO, "test_message1"));
+    EXPECT_CALL(t1, Write(WARN, "test_message2"));
+    EXPECT_CALL(t2, Write(INFO, "test_message1"));
+    EXPECT_CALL(t2, Write(WARN, "test_message2"));
+}
