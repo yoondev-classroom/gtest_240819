48d7bb661f0ef9bcd7950204122c3939ffc398dd
Chansik Yun <chansigi@gmail.com>
2024-08-20 00:38:52 +0000

    .

--- 8_비기능테스트2.cpp
+++ 8_비기능테스트2.cpp
@@ -21,6 +21,9 @@ bool DrawImage(const std::string& url)
 {
     Image* p = new Image { url };
     p->Draw();
+    delete p;
+
+    int* p2 = new int[3];
 
     // ...
     return true;
@@ -29,6 +32,12 @@ bool DrawImage(const std::string& url)
 #include <gtest/gtest.h>
 
 // 메모리 누수 여부를, 단위 테스트를 통해서 검증하고 싶습니다.
+// 방법 2. Sanitizer
+//   => 대부분의 컴파일러들이 제공하는 도구 입니다.
+//     - 메모리 / 스레드 / 미정의 동작
+//   => 모든 플랫폼에서 제공되지는 않습니다.
+//     - 컴파일 옵션: -fsanitize=address
+
 TEST(ImageTest, DrawImage)
 {
     std::string url = "https://a.com/a.jpg";
--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-g++ $1 -I ./googletest/googletest/include/ -lgtest -L. -std=c++14 -pthread
+g++ $1 -I ./googletest/googletest/include/ -lgtest -L. -std=c++14 -pthread -fsanitize=address
