1a53a3833fc477dcea6fa6987ee1d95a4bc3926e
Chansik Yun <chansigi@gmail.com>
2024-08-20 04:27:30 +0000

    .

--- 11_파라미터화테스트4.cpp
+++ 11_파라미터화테스트4.cpp
@@ -17,6 +17,14 @@ std::vector<std::string> cars = {
     "Sonata", "Avante", "Genesis"
 };
 
+std::vector<std::string> LoadFileFile()
+{
+    // file 로드
+    return {
+        "Sonata", "Avante", "Genesis"
+    };
+}
+
 // 구글 테스트의 파라미터화 테스트는 두 개 이상의 데이터 셋을 조합해서
 // 테스트를 수행할 수 있습니다.
 // => tuple
@@ -27,7 +35,8 @@ class CarTest : public testing::TestWithParam<CarType> { };
 
 INSTANTIATE_TEST_SUITE_P(CarValues, CarTest,
     testing::Combine(
-        testing::ValuesIn(cars),
+        // testing::ValuesIn(cars),
+        testing::ValuesIn(LoadFileFile()),
         testing::ValuesIn(colors),
         testing::Values(2020, 2021, 2022, 2023)));
 
