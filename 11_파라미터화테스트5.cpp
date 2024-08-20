db75f74360b20f4d755af4da811e3eb4d8f434b9
Chansik Yun <chansigi@gmail.com>
2024-08-20 04:33:10 +0000

    .

---
+++ 11_파라미터화테스트5.cpp
@@ -0,0 +1,25 @@
+// 11_파라미터화테스트5.cpp
+#include <gtest/gtest.h>
+
+// * 파라미터화 테스트에서 데이터셋을 정의하는 함수
+// 1) testing::Values(1, 2, 3, 4, 5)
+// 2) int data[] = { 1, 2, 3, 4, 5 };
+//    testing::ValuesIn(data)
+// 3) testing::Range(start, end, step)
+//    [start, end)
+// 4) testing::Combine(
+//       testing::Values(1, 2, 3, 4, 5),
+//       testing::ValuesIn(data)
+//    )
+
+class SampleTest : public testing::TestWithParam<int> { };
+
+// 1<100 => 1 ~ 99
+INSTANTIATE_TEST_SUITE_P(IntValues, SampleTest,
+    // testing::Range(1, 100));
+    testing::Range(1, 100, 5));
+
+TEST_P(SampleTest, IntTest)
+{
+    std::cout << GetParam() << std::endl;
+}
