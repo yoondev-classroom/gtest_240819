b151e611e62da086d45c4bc9265dea63bae95cc2
Chansik Yun <chansigi@gmail.com>
2024-08-20 06:38:57 +0000

    .

--- 15_Fake.cpp
+++ 15_Fake.cpp
@@ -64,15 +64,24 @@ public:
 
     User* LoadUser(const std::string& name) override
     {
-        return nullptr;
-        // return data[name];
+        // return nullptr;
+        return data[name];
     }
 };
 
 // 1) 사용자 정의 타입에 대해서, 단언문을 사용할 경우, 단언문이 사용하는 연산자에 대한 재정의가 필요합니다.
 bool operator==(const User& lhs, const User& rhs)
 {
-    return lhs.GetName() == rhs.GetName() && lhs.GetAge() == rhs.GetAge();
+    return false;
+    // return lhs.GetName() == rhs.GetName() && lhs.GetAge() == rhs.GetAge();
+}
+
+// 2) 사용자 정의 객체를 단언문을 통해 검증할 때,
+//    테스트가 실패할 경우, 원하는 형태로 표현되기 위한 연산자 재정의 함수가 필요합니다.
+std::ostream& operator<<(std::ostream& os, const User& user)
+{
+    return os << "(" << user.GetName() << ", "
+              << user.GetAge() << ")" << std::endl;
 }
 
 TEST(RepositoryTest, Save)
