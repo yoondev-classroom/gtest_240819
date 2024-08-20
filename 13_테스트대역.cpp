bccdcbabd36e1fa9d17552dac33db239aab7fe24
Chansik Yun <chansigi@gmail.com>
2024-08-20 05:21:46 +0000

    .

--- 13_테스트대역.cpp
+++ 13_테스트대역.cpp
@@ -18,11 +18,13 @@ public:
     // hello.log => hello => O
     bool IsValidLogFilename(const std::string& filename)
     {
+        //--------- 테스트 대상 코드 영역
         size_t index = filename.find_last_of(".");
         std::string name = filename.substr(0, index);
         if (name.size() < 5) {
             return false;
         }
+        //--------- 테스트 대상 코드 영역
 
         FileSystem fs;
         return fs.IsValidFilename(filename);
