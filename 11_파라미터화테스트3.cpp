c88916ec01d6ca01f7bb9985626affd4fab620f5
Chansik Yun <chansigi@gmail.com>
2024-08-20 02:45:47 +0000

    .

--- 11_파라미터화테스트3.cpp
+++ 11_파라미터화테스트3.cpp
@@ -2,7 +2,7 @@
 
 bool IsPrime(int value)
 {
-    return false;
+    // return false;
     for (int i = 2; i < value; ++i) {
         if (value % i == 0) {
             return false;
@@ -15,3 +15,29 @@ bool IsPrime(int value)
 #include <gtest/gtest.h>
 // 입력 데이터가 여러개인 경우
 // 1) tuple
+using InputType = std::tuple<int, int>;
+
+class PrimeTest : public testing::TestWithParam<InputType> { };
+
+INSTANTIATE_TEST_SUITE_P(PrimeValues, PrimeTest,
+    testing::Values(
+        InputType { 2, 4 },
+        InputType { 3, 6 },
+        InputType { 5, 10 },
+        InputType { 7, 14 },
+        InputType { 11, 22 },
+        InputType { 13, 26 }));
+
+TEST_P(PrimeTest, IsPrime_1)
+{
+    const InputType& data = GetParam();
+
+    EXPECT_TRUE(IsPrime(std::get<1>(data)));
+}
+
+TEST_P(PrimeTest, IsPrime_2)
+{
+    const InputType& data = GetParam();
+
+    EXPECT_FALSE(IsPrime(std::get<0>(data)));
+}
