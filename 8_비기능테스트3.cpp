afba28de86daeced207e43b8ca6441e019ef3b9d
Chansik Yun <chansigi@gmail.com>
2024-08-20 00:18:46 +0000

    .

---
+++ 8_비기능테스트3.cpp
@@ -0,0 +1,36 @@
+// 8_비기능테스트3.cpp
+#include <iostream>
+#include <string>
+
+class Image {
+    std::string url;
+
+public:
+    Image(const std::string& s)
+        : url { s }
+    {
+    }
+
+    void Draw() const
+    {
+        std::cout << "Draw Image: " << url << std::endl;
+    }
+};
+
+bool DrawImage(const std::string& url)
+{
+    Image* p = new Image { url };
+    p->Draw();
+
+    // ...
+    return true;
+}
+
+#include <gtest/gtest.h>
+
+// 메모리 누수 여부를, 단위 테스트를 통해서 검증하고 싶습니다.
+TEST(ImageTest, DrawImage)
+{
+    std::string url = "https://a.com/a.jpg";
+    EXPECT_TRUE(DrawImage(url));
+}
