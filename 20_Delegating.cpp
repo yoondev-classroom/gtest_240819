2ec0515f2bb0823f92c008ad9315e4b202fefa31
Chansik Yun <chansigi@gmail.com>
2024-08-21 02:26:37 +0000

    .

---
+++ 20_Delegating.cpp
@@ -0,0 +1,37 @@
+// 20_Delegating.cpp
+#include <iostream>
+using namespace std;
+
+class Calc {
+public:
+    virtual ~Calc() { }
+
+    virtual int Add(int a, int b) { return a + b; }
+    virtual int Sub(int a, int b) { return a - b; }
+};
+
+void Process(Calc* p)
+{
+    if (p->Add(10, 20) == 30) {
+        int result = p->Sub(100, 50);
+        std::cout << "result: " << result << std::endl;
+    }
+}
+
+#include <gmock/gmock.h>
+
+class MockCalc : public Calc {
+public:
+    MOCK_METHOD(int, Add, (int a, int b), (override));
+    MOCK_METHOD(int, Sub, (int a, int b), (override));
+};
+
+TEST(CalcTest, Process)
+{
+    MockCalc mock;
+
+    EXPECT_CALL(mock, Add(10, 20));
+    EXPECT_CALL(mock, Sub(100, 50));
+
+    Process(&mock);
+}
