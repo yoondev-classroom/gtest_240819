be4fcaf0454a8c909ef51fbf4dbc0dcb91e21a00
Chansik Yun <chansigi@gmail.com>
2024-08-21 05:42:34 +0000

    .

--- 21_행위기반검증2.cpp
+++ 21_행위기반검증2.cpp
@@ -31,7 +31,6 @@ void Process(Dog* p)
 //  : First -> Second -> Third -> Fourth
 
 // => testing::InSequence 객체
-
 using testing::InSequence;
 TEST(DogTest, Sample1)
 {
@@ -40,9 +39,83 @@ TEST(DogTest, Sample1)
 
     EXPECT_CALL(mock, First);
     EXPECT_CALL(mock, Second);
-
-    EXPECT_CALL(mock, Fourth);
     EXPECT_CALL(mock, Third);
+    EXPECT_CALL(mock, Fourth);
 
     Process(&mock);
+}
+
+void Process2(Dog* p)
+{
+    p->First();
+
+    p->Third();
+    p->Fourth();
+
+    p->Second();
+}
+
+// First ---> Second            ; seq1
+//       |
+//       ---> Third -> Fourth   ; seq2
+
+// => EXPECT_CALL().InSequence()
+using testing::Sequence;
+
+TEST(DogTest, Sample2)
+{
+    Sequence seq1, seq2;
+    MockDog mock;
+
+    EXPECT_CALL(mock, First).InSequence(seq1, seq2);
+    EXPECT_CALL(mock, Second).InSequence(seq1);
+
+    EXPECT_CALL(mock, Third).InSequence(seq2);
+    EXPECT_CALL(mock, Fourth).InSequence(seq2);
+
+    Process2(&mock);
+}
+
+class Car {
+public:
+    virtual ~Car() { }
+
+    virtual void f1() { }
+    virtual void f2() { }
+    virtual void f3() { }
+    virtual void f4() { }
+    virtual void f5() { }
+};
+
+class MockCar : public Car {
+public:
+    MOCK_METHOD(void, f1, (), (override));
+    MOCK_METHOD(void, f2, (), (override));
+    MOCK_METHOD(void, f3, (), (override));
+    MOCK_METHOD(void, f4, (), (override));
+    MOCK_METHOD(void, f5, (), (override));
+};
+
+void foo(Car* p)
+{
+    p->f1();
+    p->f2();
+    p->f3();
+    p->f4();
+    p->f5();
+}
+
+// f1 ---> f2
+//    |
+//    ---> f3 --> f5
+//    |
+//    ---> f4
+
+TEST(CarTest, Sample)
+{
+    MockCar mock;
+
+    // EXPECT_CALL
+
+    foo(&mock);
 }
